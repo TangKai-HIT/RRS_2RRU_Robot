348d6c815988be6471e4983838e31d885b4fb436
TangKai-HIT <1292737216@qq.com>
2023-05-10 11:13:19 +0800

    fixed bugs in static force analysis

--- RRS_2RRU_Basic.m
+++ RRS_2RRU_Basic.m
@@ -24,6 +24,15 @@ classdef RRS_2RRU_Basic < handle
         A3;
         R_P;
         TC; %tool center
+
+        %independent motion coordinate
+        z_p; alpha; beta;
+
+        %dirctional vectors
+        c11; c12;
+        c21; c22;
+        c31; c32;
+        b1; b2; b3;
     end
 
     methods
@@ -91,35 +100,38 @@ classdef RRS_2RRU_Basic < handle
             gamma = acos((d^2 + obj.L1^2 - obj.L2^2)/(2*d*obj.L1));
             thetas(3) = delta - gamma;
             
+            %record result
             obj.thetas = thetas;
 
             %update Bi
             obj.B1 = obj.A1 + eul2rotm([0, -thetas(1), 0], "ZYX") * [obj.L1; 0; 0];
             obj.B2 = obj.A2 + eul2rotm([0, -thetas(2), 0], "ZYX") * [obj.L1; 0; 0];
             obj.B3 = obj.A3 + eul2rotm([0, 0, thetas(3)], "ZYX") * [0; obj.L1; 0];
+
+            %update dirctional vectors
+            obj.b1 = (obj.B1 - obj.A1)/obj.L1;
+            obj.b2 = (obj.B2 - obj.A2)/obj.L1;
+            obj.b3 = (obj.B3 - obj.A3)/obj.L1;
+
+            obj.c11 = (obj.C1 - obj.B1)/obj.L2;
+            obj.c21 = (obj.C2 - obj.B2)/obj.L2;
+            obj.c31 = (obj.C3 - obj.B3)/obj.L2;
+
+            obj.c12 = (obj.C1 - obj.P)/obj.r;
+            obj.c22 = (obj.C2 - obj.P)/obj.r;
+            obj.c32 = (obj.C3 - obj.P)/obj.r;
         end
         
         function J_a = getActuationJacob(obj)
             %GETACTUATIONJACOB J_a*dX_p = [dtheta11; dtheta21; dtheta31]
-            b1 = (obj.B1 - obj.A1)/obj.L1;
-            b2 = (obj.B2 - obj.A2)/obj.L1;
-            b3 = (obj.B3 - obj.A3)/obj.L1;
-
-            c11 = (obj.C1 - obj.B1)/obj.L2;
-            c21 = (obj.C2 - obj.B2)/obj.L2;
-            c31 = (obj.C3 - obj.B3)/obj.L2;
-
-            c12 = (obj.C1 - obj.P)/obj.r;
-            c22 = (obj.C2 - obj.P)/obj.r;
-            c32 = (obj.C3 - obj.P)/obj.r;
 
             s1 = [0;-1;0]; %theta1
             s2 = [0;-1;0]; %theta2
             s3 = [1;0;0]; %theta3
 
-            J_1a = [obj.r*(cross(c12,c11)'), c11'] ./ (obj.L1*dot(cross(b1,c11), s1));
-            J_2a = [obj.r*(cross(c22,c21)'), c21'] ./ (obj.L1*dot(cross(b2,c21), s2));
-            J_3a = [obj.r*(cross(c32,c31)'), c31'] ./ (obj.L1*dot(cross(b3,c31), s3));
+            J_1a = [obj.r*(cross(obj.c12,obj.c11)'), obj.c11'] ./ (obj.L1*dot(cross(obj.b1,obj.c11), s1));
+            J_2a = [obj.r*(cross(obj.c22,obj.c21)'), obj.c21'] ./ (obj.L1*dot(cross(obj.b2,obj.c21), s2));
+            J_3a = [obj.r*(cross(obj.c32,obj.c31)'), obj.c31'] ./ (obj.L1*dot(cross(obj.b3,obj.c31), s3));
 
             J_a = [J_1a; J_2a; J_3a];
         end
@@ -131,8 +143,12 @@ classdef RRS_2RRU_Basic < handle
                 -sin(beta), sin(alpha)*cos(beta),  cos(alpha)*cos(beta)];
         end
 
-        function [Tf_BTC, Tf_P] = calEndEffectorSE3(obj, zp, alpha, beta)
+        function [Tf_BTC, Tf_P] = setEndEffectorSE3(obj, zp, alpha, beta)
             %CALENDEFFECTORSE3
+            obj.z_p = zp;
+            obj.alpha = alpha;
+            obj.beta = beta;
+
             R_p = obj.calEndEffectorSO3(alpha, beta);
 
             P_0 = [-sin(alpha)*sin(beta)*obj.r; 0; zp];
@@ -145,7 +161,12 @@ classdef RRS_2RRU_Basic < handle
         end
 
         function [J_rt, J_rp] = getOutputJacob(obj, alpha, beta)
-            %GETOUTPUTJACOB
+            %GETOUTPUTJACOB J_rp*[dz_p; dalpha, dbeta] = dX_p
+            if ~exist("alpha","var") && ~exist("beta","var")
+                alpha = obj.alpha;
+                beta = obj.beta;
+            end
+
             J_rp_T = [0, -cos(alpha)*sin(beta)*obj.r, -sin(alpha)*cos(beta)*obj.r;
                       0,           0,                           0;
                       1,            0,                          0];
@@ -172,14 +193,15 @@ classdef RRS_2RRU_Basic < handle
             J_a = obj.getActuationJacob();
             wrench_tool = [torque_body; force_body]; %wrench in tool center frame
             
-            %transform (adjoint map to P frame)
-            Tf_P_BTC = [obj.R_P,    [0; 0; obj.toolHight];
+            %transform (adjoint map to P frame that aligned with base frame)
+            Tf_Pa_BTC = [obj.R_P,    obj.R_P*[0; 0; obj.toolHight];
                                     zeros(1,3),           1];
 
-            wrench_P = adjointMatrix(Tf_P_BTC)' * wrench_tool;
+            wrench_P = (adjointMatrix(getInvSE3(Tf_Pa_BTC))') * wrench_tool;
+            
+            [~, J_rp] = obj.getOutputJacob();
 
-%             torque_actuate = pinv(J_a') * wrench_P;
-            torque_actuate = (J_a')\(blkdiag(obj.R_P, obj.R_P)' *  wrench_P);
+            torque_actuate = - (J_a*J_rp)' \ (J_rp' *  wrench_P);
         end
 
     end
