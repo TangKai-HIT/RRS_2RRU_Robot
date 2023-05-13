f9e275ee4c4bb050bc290139ac5553c10780a137
TangKai-HIT <1292737216@qq.com>
2023-05-13 21:24:38 +0800

    updated contents

--- RRS_2RRU_Basic.m
+++ RRS_2RRU_Basic.m
@@ -12,6 +12,7 @@ classdef RRS_2RRU_Basic < handle
     end
 
     properties(SetAccess=private, GetAccess=public)
+        %inverse kinematic points
         P;
         C1;
         C2;
@@ -25,6 +26,12 @@ classdef RRS_2RRU_Basic < handle
         R_P;
         TC; %tool center
 
+        %max & min z_p, operation height, route on z_p
+        z_p_max;
+        z_p_min;
+        z_p_operation;
+        delta_z_p;
+
         %independent motion coordinate
         z_p; alpha; beta;
 
@@ -36,7 +43,7 @@ classdef RRS_2RRU_Basic < handle
     end
 
     methods
-        function obj = RRS_2RRU_Basic(R, r, L1, L2, toolHight)
+        function obj = RRS_2RRU_Basic(R, r, L1, L2, toolHight, z_p_min)
             %RRS_2RRU_ROBOT Construct an instance of this class
             %   Detailed explanation goes here
             if nargin>0
@@ -48,6 +55,10 @@ classdef RRS_2RRU_Basic < handle
                 obj.A1 = [obj.R; 0; 0];
                 obj.A2 = [-obj.R; 0; 0];
                 obj.A3 = [0; obj.R; 0];
+                obj.z_p_max = sqrt((L1+L2)^2-(R-r)^2);
+                obj.z_p_min = z_p_min;
+                obj.z_p_operation = (obj.z_p_max + obj.z_p_min) / 2;
+                obj.delta_z_p = obj.z_p_max - obj.z_p_min;
             end
         end
 
@@ -100,6 +111,13 @@ classdef RRS_2RRU_Basic < handle
             gamma = acos((d^2 + obj.L1^2 - obj.L2^2)/(2*d*obj.L1));
             thetas(3) = delta - gamma;
             
+            %return empty if get nonvalid solution
+            if ~isreal(thetas)
+                thetas = [];
+                obj.thetas = [];
+                return;
+            end
+
             %record result
             obj.thetas = thetas;
 
@@ -125,17 +143,52 @@ classdef RRS_2RRU_Basic < handle
         function J_a = getActuationJacob(obj)
             %GETACTUATIONJACOB J_a*dX_p = [dtheta11; dtheta21; dtheta31]
 
+            [J_theta_a, J_X_a] = obj.getSplitedActuationJacob();
+
+            J_a = J_theta_a \ J_X_a;
+        end
+
+        function [J_theta_a, J_X_a] = getSplitedActuationJacob(obj)
+            %GETSPLITEDACTUATIONJACOB J_theta_a * dtheta = J_X_a * dX_a
+
             s1 = [0;-1;0]; %theta1
             s2 = [0;-1;0]; %theta2
             s3 = [1;0;0]; %theta3
 
-            J_1a = [obj.r*(cross(obj.c12,obj.c11)'), obj.c11'] ./ (obj.L1*dot(cross(obj.b1,obj.c11), s1));
-            J_2a = [obj.r*(cross(obj.c22,obj.c21)'), obj.c21'] ./ (obj.L1*dot(cross(obj.b2,obj.c21), s2));
-            J_3a = [obj.r*(cross(obj.c32,obj.c31)'), obj.c31'] ./ (obj.L1*dot(cross(obj.b3,obj.c31), s3));
-
-            J_a = [J_1a; J_2a; J_3a];
+            J_theta_a = obj.L1 * blkdiag(dot(cross(obj.b1,obj.c11), s1), dot(cross(obj.b2,obj.c21), s2), dot(cross(obj.b3,obj.c31), s3));
+            
+            J_X_1a = [obj.r*(cross(obj.c12,obj.c11)'), obj.c11'];
+            J_X_2a = [obj.r*(cross(obj.c22,obj.c21)'), obj.c21'];
+            J_X_3a = [obj.r*(cross(obj.c32,obj.c31)'), obj.c31'];
+            
+            J_X_a = [J_X_1a; J_X_2a; J_X_3a];
         end
         
+        function [forwardSingular, inverseSingular] = checkSingularity(obj, tol)
+            %CHECKSINGULARITY check inverse and forward singularity
+            forwardSingular = false;
+            inverseSingular = false;
+
+            %check forward singularity
+            forward = [norm(cross(obj.b1,obj.c11)), norm(cross(obj.b2,obj.c21)), norm(cross(obj.b3,obj.c31))];
+            if min(forward) < tol
+                forwardSingular = true;
+            end
+            
+            %check inverse singularity
+            inverse = [norm(cross(obj.c12,obj.c11)), norm(cross(obj.c22,obj.c21)), norm(cross(obj.c32,obj.c31))];
+            if min(inverse) < tol
+                inverseSingular = true;
+            end
+        end
+
+        function thetas = setToInitPose(obj)
+            %SETTOINITPOSE set To Initial Pose defined by z_p_min
+            P_pose = [obj.z_p_min, 0, 0]; %[z_p, alpha, beta]
+            [Tf_BTC, ~] = obj.setEndEffectorSE3(P_pose(1), P_pose(2), P_pose(3));
+            thetas = obj.invKineUpdate(Tf_BTC); %invkine
+        end
+
         function R_p = calEndEffectorSO3(obj, alpha, beta)
             %CALENDEFFECTORSE3
             R_p = [cos(beta),  sin(alpha)*sin(beta),  cos(alpha)*sin(beta);
@@ -185,7 +238,7 @@ classdef RRS_2RRU_Basic < handle
             J_rt = trans * J_rp;
         end
 
-        function   torque_actuate = getStaticForce_tool(obj, torque_body, force_body)
+        function torque_actuate = getStaticForce_tool(obj, torque_body, force_body)
             %GETSTATICFORCE_TOOL get equilibrium torque on actuators when setting wrench on tool center 
             %   Inputs:
             %       torque_body, force_body: 3 X 1, torque & force in local BTC frame
@@ -202,6 +255,73 @@ classdef RRS_2RRU_Basic < handle
             [~, J_rp] = obj.getOutputJacob();
 
             torque_actuate = - (J_a*J_rp)' \ (J_rp' *  wrench_P);
+        end
+
+        function f = plotToolWorkSpace(obj, zp_range, alpha_range, beta_range, N_zp)
+            %PLOTTOOLWORKSPACE plot tool tip workspace
+            N = 100;
+            zp_space = linspace(zp_range(1), zp_range(2), N_zp);
+            alpha_space = linspace(alpha_range(1), alpha_range(2), N);
+            beta_space = linspace(beta_range(1), beta_range(2), N);
+                     
+            f = figure();
+            
+            singular_tol = 0.02; %singularity check tolerance
+            cond_tol = 0.02; %condition number check tolerance
+
+            for cur_zp = zp_space
+                x_tool = [];
+                y_tool = [];
+                z_tool = [];
+                %surface on zp_i
+                for cur_alpha = alpha_space
+                    for cur_beta = beta_space
+                        %set pose & IK
+                        [Tf_BTC, ~] = obj.setEndEffectorSE3(cur_zp, cur_alpha, cur_beta);
+                        obj.invKineUpdate(Tf_BTC);
+                        
+                        %skip non valid solutions
+                        if isempty(obj.thetas)
+                            continue;
+                        end
+
+                        %singularity condition
+                        [forwardSingular, inverseSingular] = obj.checkSingularity(singular_tol);
+
+                        %condition number condition
+                        J_a = obj.getActuationJacob();
+                        [~, J_r] = obj.getOutputJacob();
+                        J = J_a*J_r;
+                        cond0 = (1/cond(J)) > cond_tol;
+
+                        %other conditions
+%                         cond1 = 
+
+                        %add new points
+                        if ~(forwardSingular || inverseSingular) && cond0
+                            x_tool = [x_tool, obj.TC(1)];
+                            y_tool = [y_tool,obj.TC(2)];
+                            z_tool = [z_tool,obj.TC(3)];
+                        end
+                    end
+                end
+
+                %plot layer on zp_i
+                if ~isempty(x_tool)
+                    [X_tool,Y_tool]=meshgrid(linspace(min(x_tool),max(x_tool),N), linspace(min(y_tool),max(y_tool),N));
+                    Z_tool=griddata(x_tool, y_tool, z_tool, X_tool, Y_tool);
+
+                    surf(X_tool,Y_tool,Z_tool,'LineStyle','none');
+                    colormap jet; shading interp; alpha 0.7;
+                    colorbar;
+
+                    set(gca,'Fontname','times new Roman','Fontsize',10);
+            %         set(gca,'XLim',[-150 150],'YLim',[-150 150],'ZLim',[200 700]);
+            %         set(gca,'XTick',[-150:75:150],'YTick',[-150:75:150],'ZTick',[200:100:700]);
+                    xlabel('Tool_x/m'),ylabel('Tool_y/m'); zlabel('Tool_z/m');
+                    hold on
+                end
+            end
         end
 
     end
